f39d47192092337c1a5b97570082151f05a8d4de
azizahmed <azizahmed@192.168.10.3>
2019-11-03 11:30:07 +0500

    1st commit

---
+++ ChainPattern/src/NegativeProcessor.java
@@ -0,0 +1,16 @@
+
+class Number 
+{ 
+    private int number; 
+  
+    public Number(int number) 
+    { 
+        this.number = number; 
+    } 
+  
+    public int getNumber() 
+    { 
+        return number; 
+    } 
+  
+} 
---
+++ ChainPattern/src/ZeroProcessor.java
@@ -0,0 +1,23 @@
+
+class ZeroProcessor implements Chain 
+{ 
+      
+    private Chain nextInChain; 
+  
+    public void setNext(Chain c) 
+    { 
+        nextInChain = c; 
+    } 
+  
+    public void process(Number request) 
+    { 
+        if (request.getNumber() == 0) 
+        { 
+            System.out.println("ZeroProcessor : " + request.getNumber()); 
+        } 
+        else
+        { 
+            nextInChain.process(request); 
+        } 
+    } 
+} 
